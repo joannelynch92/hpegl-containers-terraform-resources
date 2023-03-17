be3a44faf018010065dee676e0a2c2d44e0d29f7
Shreyas H D <91560039+shreyas-h-d@users.noreply.github.com>
2023-03-17 11:58:59 +0530

    udapte kuebernetesVersion as per CaaS Dev 2 after ERE upgarde to 5.5.1 (#74)

--- internal/acceptance_test/resource_caas_cluster_blueprint_test.go
+++ internal/acceptance_test/resource_caas_cluster_blueprint_test.go
@@ -26,7 +26,7 @@ const (
 	cpCount             = "1"
 	workerName          = "worker1"
 	workerCount         = "1"
-	kubernetesVersion   = "1.23.10-hpe2"
+	kubernetesVersion   = "1.23.13-hpe2"
 	apiURLCbp           = "https://mcaas.intg.hpedevops.net/mcaas"
 	siteNameCBp         = "FTC"
 	//apiURLCbp           = "https://mcaas.us1.greenlake-hpe.com/mcaas"
